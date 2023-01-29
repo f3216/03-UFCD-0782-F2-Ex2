561316acaf427d0871bfac8f521bb900fec6c1d9
f3216 <122741727+f3216@users.noreply.github.com>
2023-01-29 20:15:14 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,15 +6,19 @@ expected_output="Estou a aprender C."
 
 if [ $? -eq 0 ] ; then
   echo "Aprovado: o programa terminou com retorno zero"
+  echo "::set-env name=TEST_RESULT::Aprovado"
 else
   echo "Falha: o programa não retornou zero"
+  echo "::set-env name=TEST_RESULT::Falha"
   exit 1
 fi
 
 if [ "$output" == "$expected_output" ] ; then
   echo "Aprovado: A saída é correcta"
+  echo "::set-env name=TEST_OUTPUT::Aprovado"
 else
   echo "Esperada a saída '$expected_output' mas o programa devolveu: $output"
+  echo "::set-env name=TEST_OUTPUT::Falha"
   exit 1
 fi
 
